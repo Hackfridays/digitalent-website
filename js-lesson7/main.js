e19ddbcfca288bcb8f5d7f9fa900f2fed64f9761
Koen Betsens <betsens@Koens-MacBook-Air.local>
2017-11-17 10:57:48 +0100

    Added GUA

--- js-lesson7/main.js
+++ js-lesson7/main.js
@@ -1,7 +1,9 @@
 var myHeading = document.querySelector('h1');
 
+
 $('#button').on('click', function (){
 
+  var myInputText = document.getElementById('textInput').value;
 
-  myHeading.textContent = "Thanks";
+  myHeading.textContent = myInputText;
 })
